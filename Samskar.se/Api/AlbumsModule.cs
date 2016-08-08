b643464f85649cb0dab0ff90e8da782eb18f185f
Mattias Sämskar <mattias.samskar@gmail.com>
2016-08-08 16:17:14 +0200

    Add CreateAlbum to API

--- Samskar.se/Api/AlbumsModule.cs
+++ Samskar.se/Api/AlbumsModule.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using MediatR;
 using Nancy;
+using Nancy.ModelBinding;
+using Samskar.Commands;
 using Samskar.Queries;
 
 namespace Samskar.Api
@@ -15,6 +17,12 @@ namespace Samskar.Api
 
             Get["/"] = _ => GetAlbums();
             Get["/{name}"] = _ => GetAlbum(_.name);
+            Post["/"] = _ => CreateAlbum(this.Bind<CreateAlbumCommand>());
+        }
+
+        private dynamic CreateAlbum(CreateAlbumCommand createAlbumCommand)
+        {
+            return _mediator.Send(createAlbumCommand);
         }
 
         private IEnumerable<string> GetAlbums()

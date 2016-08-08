da812a9e9c0063839b54e1055c9e3985b96fa772
Mattias Sämskar <mattias.samskar@gmail.com>
2016-08-08 10:46:21 +0200

    Add ImagesModule

---
+++ Samskar.se/ImagesModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Nancy;
+
+namespace Samskar
+{
+    public class ImagesModule : NancyModule
+    {
+        public ImagesModule() : base("/images")
+        {
+            Get["/"] = _ => GetAllImages();
+        }
+
+        private IEnumerable<Image> GetAllImages()
+        {
+            return new List<Image>
+            {
+                new Image {Url = "http://images.samskar.se/1.jpg"},
+                new Image {Url = "http://images.samskar.se/2.jpg"},
+                new Image {Url = "http://images.samskar.se/3.jpg"}
+            };
+        }
+    }
+
+    public class Image
+    {
+        public string Url { get; set; }
+    }
+}

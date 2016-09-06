cb03e551f303e1577a11aec3dbc878895fdbdda2
Mattias Sämskar <mattias.samskar@gmail.com>
2016-09-06 19:26:47 +0200

    Change namespace to Samskar. Add project for backend unit tests.

---
+++ src/backend/Samskar.Tests/AlbumsCommandHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NSubstitute;
+using Samskar.CommandHandlers;
+using Samskar.Commands;
+using Samskar.Notifications;
+using Xunit;
+
+namespace Samskar.Tests
+{
+    public class AlbumsCommandHandlerTests
+    {
+        [Fact]
+        public void Should_Notify_When_Creating_Album()
+        {
+            // Arrange
+            var mediator = Substitute.For<IMediator>();
+            var handler = new AlbumsCommandHandler(mediator);
+
+            // Act
+            handler.Handle(new CreateAlbumCommand {Name = "name"});
+
+            // Assert
+            mediator.Received().Publish(Arg.Is<AlbumCreatedNotification>(n => n.Name == "name"));
+        }
+    }
+}

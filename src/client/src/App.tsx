da3e4f4ef35269f11111282c926f003c4bf6b972
Mattias Sämskar <mattias.samskar@gmail.com>
2019-07-24 16:48:20 +0200

    Add azure-storage

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import { UserAgentApplication, AuthenticationParameters } from "msal";
 
+const userAgentApplication = new UserAgentApplication({
+  auth: {
+    clientId: "75a0a84b-3082-4a1c-9bdd-c4652ef3dbe4",
+    authority:
+      "https://login.microsoftonline.com/51f829d7-e997-4ba6-ab64-0797bcb49176",
+    redirectUri: "http://localhost:3000/"
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true
+  }
+});
+
 const App: React.FC = () => {
   const [accessToken, setAccessToken] = useState("");
   const [name, setName] = useState("");
   const [folders, setFolders] = useState([]);
 
   const getAccessToken = async () => {
-    const userAgentApplication = new UserAgentApplication({
-      auth: {
-        clientId: "75a0a84b-3082-4a1c-9bdd-c4652ef3dbe4",
-        authority:
-          "https://login.microsoftonline.com/51f829d7-e997-4ba6-ab64-0797bcb49176",
-        redirectUri: "http://localhost:3000"
-      },
-      cache: {
-        cacheLocation: "localStorage",
-        storeAuthStateInCookie: true
-      }
-    });
-
     const authenticationParameters: AuthenticationParameters = {
-      scopes: ["user.read"]
+      scopes: ["https://storage.azure.com/user_impersonation"]
     };
 
     try {
@@ -39,8 +39,17 @@ const App: React.FC = () => {
     }
   };
 
-  const getFolders = () => {
-    const url = "https://samskarstorage.blob.core.windows.net/samskar-se";
+  const getFolders = async () => {
+    const url =
+      "https://samskarstorage.blob.core.windows.net/samskar-se?restype=container&comp=list&delimiter=/";
+
+    const folders = await fetch(url, {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+        "x-ms-version": "2017-11-09"
+      }
+    });
+    console.log(folders);
   };
 
   return (
